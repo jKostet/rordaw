8cfa0b48677b372cf14fe0e06e1968e103da3b88
jKostet <juho.kostet@gmail.com>
2015-02-15 20:58:45 +0200

    baarihaku

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,11 @@ Rails.application.routes.draw do
   resources :ratings, only: [:index, :new, :create, :destroy]
 
 
+  get 'places', to: 'places#index'
+  post 'places', to:'places#search'
+
+
+
   root 'breweries#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
